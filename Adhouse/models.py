41b0a155ba2a11bd610f912439fe168f13507a09
a-shamasneh <a.ashmasneh@gmail.com>
2017-05-11 20:46:51 +0300

    connect with mongodb and create model for user

--- Adhouse/models.py
+++ Adhouse/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+from mongoengine import *
 # Create your models here.
+class User(Document):
+    
+    u_name = StringField(max_length=50)
+    u_pass = StringField(max_length=50)
+    u_email= StringField(required=True)
+    u_phone= StringField(required=True)
+    
--- Adhouse/views.py
+++ Adhouse/views.py
@@ -2,10 +2,19 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-
+from models import User
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
+    user=User.objects.create(
+    	email="a.ashamasneh@gmail.com",
+    	first_name="Ahmad",
+    	last_name="Shamasneh"
+
+    	)
+    user.save()
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
